3ca8e0334df5c316dc592f6a27821cb4c235f7a6
Maycon Oliveira <rick.maycon.98@gmail.com>
2022-12-20 19:53:10 -0300

    feat: implement controller using in memory repository

--- tests/Feature/ComedianControllersTest.php
+++ tests/Feature/ComedianControllersTest.php
@@ -2,7 +2,23 @@
 
 namespace Tests\Feature;
 
-class ComedianControllersTest
+use App\Http\Controllers\GetComedianByIdController;
+use App\Http\Controllers\ListAttractionsByComedianController;
+use Illuminate\Http\Request;
+
+class ComedianControllersTest extends FeatureTestCase
 {
+
+    public function testMustBeReturn200OnListAttractionsByComedianController()
+    {
+        $request = new Request();
+        $request->comedian = 'any_id_1';
+
+        $controller = new GetComedianByIdController();
+        $response = $controller->handle($request);
+
+        $this->assertSame(200, $response->getData()->status);
+        $this->assertIsArray($response->getData()->data);
+    }
 
 }
