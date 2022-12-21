f8a8819e0364a3b4064da7032761d37bfdb3dabc
Maycon Oliveira <rick.maycon.98@gmail.com>
2022-12-20 22:06:50 -0300

    feat: create migrations

---
+++ database/migrations/2022_12_21_004854_create_attractions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attractions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('title');
+            $table->dateTime('date');
+            $table->string('duration');
+            $table->foreignUuid('artist_id');
+            $table->foreignUuid('place_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attractions');
+    }
+};
