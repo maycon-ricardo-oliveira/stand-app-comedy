115020080aa1640f4af541ab27455de10fe427ff
Maycon Oliveira <rick.maycon.98@gmail.com>
2023-01-25 19:35:05 -0300

    feat: add migrations

---
+++ database/migrations/2023_01_25_222813_create_user_follows.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_follows', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('user_id');
+            $table->uuid('comedian_id');
+            $table->timestamps();
+
+            $table->foreignUuid('comedian_id')->constrained('comedians');
+            $table->foreignUuid('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_follows');
+    }
+};
