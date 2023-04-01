781e8df90de10954886ead076969a5aa4e72f1c0
Maycon Oliveira <rick.maycon.98@gmail.com>
2023-04-01 09:30:00 -0300

    feat: create session status exception

--- app/Chore/Modules/Sessions/Exceptions/InvalidSessionStatusException.php
+++ app/Chore/Modules/Sessions/Exceptions/InvalidSessionStatusException.php
@@ -6,7 +6,7 @@ use Throwable;
 
 class InvalidSessionStatusException extends \Exception
 {
-    public function __construct(string $message = 'Invalid attraction status', int $code = 0, Throwable $previous = null) {
+    public function __construct(string $message = 'Invalid session status', int $code = 0, Throwable $previous = null) {
         parent::__construct($message, $code, $previous);
     }
 
