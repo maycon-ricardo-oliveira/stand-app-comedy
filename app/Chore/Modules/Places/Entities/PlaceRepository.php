32ba65df3c437a96f120878e19b238b0e9b238e4
Maycon Oliveira <rick.maycon.98@gmail.com>
2024-02-21 07:51:14 -0300

    feat: exception when place are already registered

--- app/Chore/Modules/Places/Entities/PlaceRepository.php
+++ app/Chore/Modules/Places/Entities/PlaceRepository.php
@@ -5,6 +5,7 @@ namespace App\Chore\Modules\Places\Entities;
 interface PlaceRepository
 {
     public function getPlaceById(string $id);
+    public function getPlaceByName(string $name): ?Place;
 
     public function register(Place $place): bool;
 
