4251a01ff0565ffbfcd17854e29c0dca2b03d80c
Maycon Oliveira <rick.maycon.98@gmail.com>
2024-07-14 12:49:32 -0300

    feat: create hot content model and migration

---
+++ database/migrations/2024_07_14_154303_create_hot_contents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hot_contents', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('content_type', 100)->nullable();
+            $table->string('content_id', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hot_contents');
+    }
+};
